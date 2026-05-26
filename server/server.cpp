f3782d1cb406ebffee4b54c2302b280bd81e822a
KartikNaik6 <kartikmnaik222@gmail.com>
2026-05-26 23:51:41 +0530

    implemented inital working TCP socket

--- server/server.cpp
+++ server/server.cpp
@@ -5,14 +5,28 @@
 #include <cstring>
 
 int main() {
-//what is a socket?: kernal managed endpoint 
+    //what is a socket?: kernal managed endpoint 
     int server_fd = socket(AF_INET, SOCK_STREAM, 0); //variable accepting file descriptor as an integer in return type
+    sockaddr_in server_addr{}; //kernal expects struct variable
+    server_addr.sin_family = AF_INET;    
+    server_addr.sin_port = htons(8080);  
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
+    //socket() asks kernal to create a socket for it which speaks IPv4 (AF_NET) and is stream oriented (SOCK_STREAM)
+    //in linux everything is treated as a stream hence file descriptors are used
     if (server_fd < 0) {
         std::cerr << "Socket creation failed\n";
         return 1;
     }
+    //error handling
 
+    bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr)); // bind the socket and address structure
+    listen(server_fd,5);
+    while(true){
+        //dumb fix
+        sleep(1);
+    }
+    
     std::cout << "Socket created\n";
 
     return 0;
